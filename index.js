0844dfabcf2e04a3b22a2db2cb432df2d1c5ec4d
Redoan Hasan <redoanhasan57@gmail.com>
2025-01-03 00:18:30 +0600

    initial project setup with database

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ app.use(cors());
 app.use(express.json());
 
 
-
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhz4s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
